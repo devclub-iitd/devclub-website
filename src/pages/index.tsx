b5dc5312815ce34b30a6958c0bcc4d6943e7e4f5
manogyasingh <manogyasinghsuryansh@gmail.com>
2025-01-17 02:52:39 +0530

    fix an oopsie

---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import Background from '../components/Background';
+import { Github, ExternalLink } from 'lucide-react';
